6166d0d1a395490ce0f501180dda5e317b5544c0
CCTV-1 <script.tar.gz@gmail.com>
2022-10-06 19:22:39 +0800

    next client version.

--- MTGAAssetDownloader.py
+++ MTGAAssetDownloader.py
@@ -10,7 +10,7 @@ class MTGA_PLATFORM(enum.Enum):
     MacOS = 'macos'
 
 playerId = "\0" #seem can be any Non-None value
-clientVersion = "2022.19.11"
+clientVersion = "2022.20.0"
 platformKey = MTGA_PLATFORM.MacOS.value
 
 if __name__ == "__main__":
