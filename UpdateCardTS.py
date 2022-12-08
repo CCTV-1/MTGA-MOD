44884635899e76343b833dcdc2560df9636a78f3
CCTV-1 <script.tar.gz@gmail.com>
2022-12-08 19:37:09 +0800

    for each non-token card,if oracle title text equal oracle subtype text,use title text translation.the subtype text translation separately insert.

--- UpdateCardTS.py
+++ UpdateCardTS.py
@@ -6,6 +6,26 @@ import sqlite3
 import Config
 import TSFile
 
+# LocId : new row
+ExtraLocalizationTexts: dict[int:dict[str, str]] = {
+    9000000: {'enUS': 'Dragon Turtle', 'jaJP': '龙／龟'},
+    9000001: {'enUS': 'Manticore', 'jaJP': '翼狮'},
+    9000002: {'enUS': 'Zombie Ogre', 'jaJP': '灵俑／食人魔'},
+    9000003: {'enUS': 'Skeleton Archer', 'jaJP': '骷髅妖／弓箭手'},
+    9000004: {'enUS': 'Dragon Egg', 'jaJP': '龙／蛋'},
+    9000005: {'enUS': 'Urza\'s Mine', 'jaJP': '克撒的／矿脉'},
+    9000006: {'enUS': 'Urza\'s Tower', 'jaJP': '克撒的／塔'},
+}
+
+# old SubtypeTextId : new SubtypeTextId
+SubtypeTextPatchRules: dict[int:int] = {
+    498439: 9000000,
+    20912: 9000001,
+    286058: 9000002,
+    229573: 9000003,
+    42935: 9000004
+}
+
 if __name__ == "__main__":
     if not Config.BACKUP_DIR.is_dir():
         Config.BACKUP_DIR.unlink(missing_ok=True)
@@ -59,13 +79,25 @@ if __name__ == "__main__":
             cardDBCursor.execute('INSERT INTO Localizations(LocId, Formatted, KnownTitleId, enUS, jaJP) VALUES(:LocId, :Formatted, :KnownTitleId, :enUS, :jaJP);',
                                  {'LocId': rawKey, 'Formatted': rawValue['Formatted'], 'KnownTitleId': rawValue['KnownTitleId'], 'enUS': rawValue['enUS'], 'jaJP': rawValue['jaJP']})
 
+        # patch extra loc texts
+        for locId, row in ExtraLocalizationTexts.items():
+            cardDBCursor.execute('INSERT INTO Localizations(LocId, Formatted, KnownTitleId, enUS, jaJP) VALUES(:LocId, :Formatted, :KnownTitleId, :enUS, :jaJP);',
+                                 {'LocId': locId, 'Formatted': 1, 'KnownTitleId': 1, 'enUS': row['enUS'], 'jaJP': row['jaJP']})
+
+        # remove pre-8ed card style
+        cardDBCursor.execute('UPDATE Cards SET AdditionalFrameDetails  = \'\' WHERE ExpansionCode = "BRR";')
+
+        # patch the conflicting SubtypeTextId
+        for oldKey, NewKey in SubtypeTextPatchRules.items():
+            cardDBCursor.execute('UPDATE Cards SET SubtypeTextId = ? WHERE SubtypeTextId = ?;', (NewKey, oldKey))
+
         # 'GC' database file
         cardDBConnect.isolation_level = None
         cardDBCursor.execute('VACUUM')
         cardDBConnect.commit()
 
     sortTS = collections.OrderedDict()
-    for i in sorted(TSInfo, key=lambda x:int(x)):
+    for i in sorted(TSInfo, key=lambda x: int(x)):
         sortTS[i] = TSInfo[i]
 
     TSFile.SaveTSInfo(sortTS, 'CardTS', TSFile.TSFileType.JSON)
