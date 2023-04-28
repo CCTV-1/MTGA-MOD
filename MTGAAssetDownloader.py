4086bb6771a6b6787960a7d5c1028d9bd2762fa8
CCTV-1 <script.tar.gz@gmail.com>
2023-04-28 18:28:24 +0800

    now has some Assetbundles are not compressed.

--- MTGAAssetDownloader.py
+++ MTGAAssetDownloader.py
@@ -12,7 +12,7 @@ class MTGA_PLATFORM(enum.Enum):
 
 
 playerId = "\0"  # seem can be any Non-None value
-clientVersion = "2022.20.10"
+clientVersion = "2023.25.10"
 platformKey = MTGA_PLATFORM.MacOS.value
 
 if __name__ == "__main__":
@@ -34,21 +34,29 @@ if __name__ == "__main__":
     unCompContent = gzip.decompress(resp.content)
     manifestJson = json.loads(unCompContent)
 
-    rawAssetNames = []
+    rawAssetInfo = {}
     for assetNode in manifestJson['Assets']:
         # Raw_CardDatabase_ Raw_ClientLocalization_ shared 'Raw_C' and Raw_cards_ not use.
         if assetNode['Name'].startswith('Raw_C'):
             pass
         else:
             continue
-        # print(assetNode['Name'])
-        rawAssetNames.append(assetNode['Name'])
 
-    for name in rawAssetNames:
-        rawAssetUri = "https://assets.mtgarena.wizards.com/{0}.gz".format(name)
-        resp = requests.get(rawAssetUri)
+        name = assetNode['Name']
+        if assetNode.get('wrapper', 'none') == 'gz':
+            rawAssetInfo[name] = "https://assets.mtgarena.wizards.com/{0}.gz".format(
+                name)
+        else:
+            rawAssetInfo[name] = "https://assets.mtgarena.wizards.com/{0}".format(
+                name)
+
+    for name, uri in rawAssetInfo.items():
+        resp = requests.get(uri)
         if resp.status_code != 200:
-            print("can not download:'{0}'".format(rawAssetUri))
+            print("can not download:'{0}'".format(uri))
             continue
         with open(name, 'wb') as f:
-            f.write(gzip.decompress(resp.content))
+            if uri.endswith('gz'):
+                f.write(gzip.decompress(resp.content))
+            else:
+                f.write(resp.content)
