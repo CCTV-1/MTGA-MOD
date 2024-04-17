dae38ccb6dcecac19d13d4aea9d1ac452927c2f9
CCTV-1 <script.tar.gz@gmail.com>
2024-04-17 16:12:07 +0800

    FieldFont asset path changed.

--- FontPatcher.py
+++ FontPatcher.py
@@ -17,7 +17,7 @@ WINDOWS_FONT_RULES: dict[str:list] = {
     '{0}/sharedassets0.assets'.format(Config.WINDOWS_DATA_DIR): [
         'Font_Default', 'Font_Title'
     ],
-    '{0}/Downloads/AssetBundle/Bucket_Card.FieldFont_0_1e24d521-2d1b1c2bd8fd43cc703c1109c3d5a75c.mtga'.format(Config.WINDOWS_DATA_DIR): [
+    '{0}/Downloads/AssetBundle/Bucket_Card.FieldFont_0_142dcc4e-e1a9a14eeab7618b03e59e3e516373c4.mtga'.format(Config.WINDOWS_DATA_DIR): [
         'Font_Default', 'Font_Title', 'Font_Default_JP', 'Font_Title_JP'
     ],
     '{0}/Downloads/AssetBundle/Fonts_d96b2579-ee3e4a0e8524fd0e79f47a0f4b81ba45.mtga'.format(Config.WINDOWS_DATA_DIR): [
@@ -46,7 +46,7 @@ MACOS_FONT_RULES: dict[str:list] = {
     '{0}/sharedassets0.assets'.format(Config.MACOS_RES_DIR): [
         'Font_Default', 'Font_Title'
     ],
-    '{0}/Downloads/AssetBundle/Bucket_Card.FieldFont_0_34f71954-2d1b1c2bd8fd43cc703c1109c3d5a75c.mtga'.format(Config.MACOS_DATA_DIR): [
+    '{0}/Downloads/AssetBundle/Bucket_Card.FieldFont_0_561c2f5a-e1a9a14eeab7618b03e59e3e516373c4.mtga'.format(Config.MACOS_DATA_DIR): [
         'Font_Default', 'Font_Title', 'Font_Default_JP', 'Font_Title_JP'
     ],
     '{0}/Downloads/AssetBundle/Fonts_c6b18957-ee3e4a0e8524fd0e79f47a0f4b81ba45.mtga'.format(Config.MACOS_DATA_DIR): [
@@ -82,7 +82,7 @@ ANDROID_FONT_RULES: dict[str:list] = {
     '{0}/sharedassets0.assets'.format(Config.ANDROID_DATA_DIR): [
         'Font_Default', 'Font_Title'
     ],
-    '{0}/AssetBundle/Bucket_Card.FieldFont_0_9b96a0b8-2d1b1c2bd8fd43cc703c1109c3d5a75c.mtga'.format(Config.ANDROID_DATA_DIR): [
+    '{0}/AssetBundle/Bucket_Card.FieldFont_0_680c8e56-e1a9a14eeab7618b03e59e3e516373c4.mtga'.format(Config.ANDROID_DATA_DIR): [
         'Font_Default', 'Font_Title', 'Font_Default_JP', 'Font_Title_JP'
     ],
     '{0}/AssetBundle/Fonts_cc07ed4d-ee3e4a0e8524fd0e79f47a0f4b81ba45.mtga'.format(Config.ANDROID_DATA_DIR): [
